1f074a80924737be2eac66872cf8ebc1c17aa158
Chackapon <30863200+Chackapon@users.noreply.github.com>
2017-08-09 14:03:57 +0300

    Fixed 0.1.1 a bit, so this update is 0.1.1.1

--- alpha-0-1-1.py
+++ alpha-0-1-1.py
@@ -1,4 +1,4 @@
-print('Welcome to ElectrOS Py Edition, version 0.1.0.1 ALPHA')
+print('Welcome to ElectrOS Py Edition, version 0.1.1 ALPHA')
 devMode = [0]
 def ElectrOScript():
   importCode = "null"
@@ -15,19 +15,19 @@ def ElectrOScript():
   text = '\n'.join(lines)
   importCode = text.split()
   if importCode[0] == "{start}":
-  while importCode[x] != "{end}":
-    if "say" in importCode[x]:
-      EScriptLine = list(importCode[x])
-      sayChars = [":)"]
-      if EScriptLine[3] == "(":
-        sayCharCounter = 4
-        output = [""]
-        while EScriptLine[sayCharCounter] != ")":
-          output.append(EScriptLine[sayCharCounter])
-          sayCharCounter += 1
-        print(''.join(output))
-        break
-  x = x + 1
+    while importCode[x] != "{end}":
+      if "say" in importCode[x]:
+        EScriptLine = list(importCode[x])
+        sayChars = [":)"]
+        if EScriptLine[3] == "(":
+          sayCharCounter = 4
+          output = [""]
+          while EScriptLine[sayCharCounter] != ")":
+            output.append(EScriptLine[sayCharCounter])
+            sayCharCounter += 1
+          print(''.join(output))
+          break
+    x = x + 1
 def OS():
   
   print('Type any command. Type .help to show help')
@@ -36,14 +36,15 @@ def OS():
     if cmd == '.help':
       print('==============================' * 2)
       print('.info - displays info about the OS\n.help - displays help\n.bot - launches ChatBot')
+      print('.import - imports ElectrOScript app\n')
       #print(devMode[0])
       if devMode[0] == 1:
-        print('.login - Login to an account\n.reg - Registers an account\n.import - imports ElectrOScript app')
+        print('.login - Login to an account\n.reg - Registers an account')
         print('.language - Changes language of the OS\n.debug - launches debug screen\n.open - opens app')
       print('==============================' * 2)
     elif cmd == '.info':
       print('==============================' * 2)
-      print('ElectrOS ALPHA\nVersion: 0.1.0.1\nEdition: Py (Python/Pi (?)/Raspberry Pi (?))')
+      print('ElectrOS ALPHA\nVersion: 0.1.1\nEdition: Py (Python/Pi (?)/Raspberry Pi (?))')
       print('Bot Version: 0.1.1\ElectrOScript Version: 0.1.1\n)')
       print('==============================' * 2)
     elif cmd == '.bot':
